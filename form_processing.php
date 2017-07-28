e000345e0867839f641bb5157b577b41ad986515
ksnurnicyn <ksnurnicyn@ksnurnicyn-pc.thumbtack.lo>
2017-07-28 11:14:59 +0600

    change header and some fix

---
+++ form_processing.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	$Name = $_POST['name'];
+	$Phone = $_POST['telephone'];
+	$Mail = $_POST['email'];
+
+	$dateNow = date("Y-m-d");
+
+	$message = "
+	Заявка на размещение рекламы
+	Имя: ".htmlspecialchars($Name)."
+	Телефон: ".htmlspecialchars($Phone)."
+	Почта: ".htmlspecialchars($Mail);
+
+	mail("8101980@mail.ru", "Заявка на интенсив от Струве", $message);
+    mail("ks_on_v@mail.ru", "Заявка на интенсив от Струве", $message);
+ 
+?>	
